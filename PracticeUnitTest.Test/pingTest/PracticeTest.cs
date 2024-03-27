07430215e44d84939e7d1a684a55a4bbb677ebb1
hasan50 <hasanakib27@gmail.com>
2024-03-28 02:07:10 +0600

    adding unit test

---
+++ PracticeUnitTest.Test/pingTest/PracticeTest.cs
@@ -0,0 +1,43 @@
+﻿
+using FluentAssertions;
+using PracticeUnitTest.ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeUnitTest.Test.pingTest
+{
+    public class PracticeTest
+    {
+        [Fact]
+       public void Practice_practiceString_returnString()
+        {
+            //Arrange
+            Practice objPractice = new Practice();
+            //Act
+            string result = objPractice.practiceString();
+            //Assart
+
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("This is a practice string");
+            result.Should().Contain("This is");
+
+        }
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,3,5)]
+        public void Practice_AddNumber_returnInt(int number1, int number2,int expected) 
+        {
+            //Arrange
+
+            Practice objPractice = new Practice();
+            //Act
+            int result=objPractice.AddNumber(number1, number2);
+            //Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+        }
+    }
+}
---
+++ PracticeUnitTest/ping/Practice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeUnitTest.ping
+{
+    public class Practice
+    {
+        public string practiceString()
+        {
+            return "This is a practice string";
+        }
+
+        public int AddNumber(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
